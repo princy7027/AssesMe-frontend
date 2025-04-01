e7f9744165d5c4ac41728f049bacc7a7ffe170a0
princy7027 <princy702702@gmail.com>
2025-04-01 20:10:08 +0530

    set home page

---
+++ src/pages/Home/Component/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+            <div className="flex items-center justify-center bg-[#FF884D] text-white text-xl h-12">@ Copyright:AssessMe.com</div>
+        </>
+  )
+}
+
+export default Footer
